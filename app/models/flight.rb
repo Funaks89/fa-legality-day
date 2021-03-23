82282613018e2b62bcb38d1e4e1b6b9f213bd349
Ken Funakoshi <funaks@live.com>
2021-03-23 17:46:29 +0900

    set some model settings

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,3 @@
 class Flight < ApplicationRecord
+  belongs_to :schedule, class_name: "schedule", foreign_key: "schedule_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :flights, class_name: "flight", foreign_key: "reference_id"
+  has_many :schedules, class_name: "schedule", foreign_key: "reference_id"
 end
